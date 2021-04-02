b1912012346e7022db372ad4de8a4fac0bed08d4
harrisonblackburn <blackbhr@yahoo.com>
2021-04-02 14:42:31 -0500

    homework stuff

---
+++ hw3d/script.js
@@ -0,0 +1,39 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//initialize empty string for password input
+
+
+
+function writePassword() {
+  console.log ("harrison")
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  let password = "";
+  $("#generate").one("click", generatePassword() {
+    // Add content...
+});
+  
+
+ 
+  var passwordLength = parseInt(
+    prompt("Please enter the length of the password")
+  );
+
+console.log(passwordLength);
+console.log(typeof passwordLength);
+ 
+if(passwordLength < 8) {
+  alert ("Password should be at least 8 characters");
+}
+else if(passwordLength > 128){
+  alert ("Please enter a password that does not exceed 128 characters");
+}
+
+
+length = passwordLength
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
